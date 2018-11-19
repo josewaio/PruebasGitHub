3aa3096aa49d907aa852793ae8054ae14cfb86b8
Unknown <Jose_waio@hotmail.com>
2018-11-19 10:50:06 +0100

    Commit 5
    Añadir k

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,12 +2,12 @@
 public class Prueba {
 
 	public static void main(String[] args) {
-		int i = 2;
+		int i = 2, k = 1;
 		boolean exit = false;
 
 		if (i >= 1) {
 			System.out.println(exit);
-		} else {
+		} else if (k >= 1) {
 			System.out.println(i);
 		}
 	}
