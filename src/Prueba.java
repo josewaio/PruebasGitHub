def254682f8f666fccf3fec4c9bd73307e65b449
Unknown <Jose_waio@hotmail.com>
2018-11-19 11:15:54 +0100

    Commit 7
    Hemos añadido la k

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,13 +2,15 @@
 public class Prueba {
 
 	public static void main(String[] args) {
-		int i = 2, j = 1;
+		int i = 2, j = 1, k = 2;
 		boolean exit = false;
 
 		if (i >= 1) {
 			System.out.println(exit);
 		} else if (j >= 1) {
 			System.out.println(i);
+		} else if (k == 2) {
+			System.out.println(k);
 		}
 	}
 
